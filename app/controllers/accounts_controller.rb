788ab4889b1594946ffff0a40ad99bc623483b94
watsjk <6734713+watsjk@users.noreply.github.com>
2024-05-12 20:41:35 +0700

    Create Account Route & Controller

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,53 @@
+class AccountsController < ApplicationController
+  before_action :set_account, only: %i[ show update destroy ]
+
+  # GET /accounts
+  def index
+    @accounts = Account.all
+
+    render json: @accounts
+  end
+
+  # GET /accounts/1
+  def show
+    render json: @account
+  end
+
+  # POST /accounts
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      render json: @account, status: :created, location: @account
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /accounts/1
+  def update
+    if @account.update(account_params)
+      render json: @account
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /accounts/1
+  def destroy
+    @account.destroy!
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_account
+    @account = Account.find(params[:id])
+    rescue ActiveRecord::RecordNotFound then render status: :not_found
+  end
+
+  # Only allow a list of trusted parameters through.
+  def account_params
+    params.fetch(:account, {}).permit(:uname, :email, :password)
+  end
+end
